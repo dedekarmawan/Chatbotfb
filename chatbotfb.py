ad1d71a0cff7e8b7b8329bfe032bf15258b6ad27
dedekarmawan <putudedekarmawan@gmail.com>
2019-10-15 23:56:16 +0800

    newnew

--- chatbotfb.py
+++ chatbotfb.py
@@ -1,5 +1,4 @@
 from flask import Flask, request, jsonify
-import sys
 import os
 import json
 import pymysql.cursors
@@ -13,34 +12,29 @@ def webhook():
     intent_name = data.get("queryResult").get("intent").get("displayName")
     inbox = data['queryResult']['queryText']
     print(data)
+
     connection = pymysql.connect(host='db4free.net',
                                  user='dedekarmawan',
                                  password='Superdede',
                                  db='snaptravelbot',
                                  charset='utf8mb4',
                                  cursorclass=pymysql.cursors.DictCursor)
-
-    try:
-        #result = None
-        with connection.cursor() as cursor:
-            sql = "INSERT INTO tb_inbox (pesan,date) VALUES (%s, %s)"
-            cursor.execute(sql, (inbox, date.today().strftime("%Y-%m-%d")))
-            # idterakhir = cursor.lastrowid
-            # sql = "INSERT INTO tb_outbox(id_inbox, pesan, date) VALUES (%s, %s, %s)"
-            # cursor.execute(sql, (idterakhir, salam(data), date.today().strftime("%Y-%m-%d")))
-        connection.commit()
-    finally:
-        connection.close()
-
     if intent_name == "salam":
         return salam(data)
 
-            # result = cursor.fetchone()
+    try:
+        with connection.cursor() as cursor:
+            sql = "INSERT INTO tb_inbox (pesan,date) VALUES (%s, %s)"
+            cursor.execute(sql, (inbox, date.today().strftime("%Y-%m-%d")))
+            idterakhir = cursor.lastrowid
+            sql = "INSERT INTO tb_outbox(id_inbox, pesan, date) VALUES (%s, %s, %s)"
+            cursor.execute(sql, (idterakhir, salam(data), date.today().strftime("%Y-%m-%d")))
+        connection.commit()
 
+    finally:
+        connection.close()
 
-    # return jsonify(request.get_json())
-
-def salam(data):
+  def salam(data):
     response = {
         'fulfillmentText':"Hai, saya Tutlesbot. Chatbot yang akan membantu anda dalam mencari hotel ketika anda berlibur. Ketik booking untuk memilih opsi kamar hotel."
     }
