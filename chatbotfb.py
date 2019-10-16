a7c7d752c1cb40d928ca6e96503c0c3b58bb1522
dedekarmawan <putudedekarmawan@gmail.com>
2019-10-16 12:49:28 +0800

    update

--- chatbotfb.py
+++ chatbotfb.py
@@ -44,7 +44,7 @@ def salam(data):
     with connection.cursor() as cursor:
         sql = "INSERT INTO tb_outbox (id_inbox, respon) VALUES (%s, %s)"
         cursor.execute(sql, (id_inbox, "Hai, saya Tutlesbot. Chatbot yang akan membantu anda dalam mencari hotel ketika anda berlibur. Ketik booking untuk memilih opsi kamar hotel."))
-        sql = "UPDATE tb_inbox SET tb_inbox.status = '1' WHERE tb_inbox.id = %s"
+        sql = "UPDATE tb_inbox SET tb_inbox.status = 1 WHERE tb_inbox.id = %s"
         cursor.execute(sql, (id_inbox))
     connection.commit()
 
